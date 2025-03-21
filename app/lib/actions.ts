b0c88b4ef0b5a717349c85c4a725ffdec35469ca
AxelSparta <axelnicolassparta@gmail.com>
2025-03-20 21:57:57 -0300

    updating invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,6 +32,26 @@ export async function createInvoice (formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
 
-	revalidatePath('/dashboard/invoices')
-	redirect('/dashboard/invoices')
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice (id: string, formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+    WHERE id = ${id}
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
 }

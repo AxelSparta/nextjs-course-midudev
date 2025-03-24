5c5172cdc1208662c36e9c416cc4ec2242ded9fb
AxelSparta <axelnicolassparta@gmail.com>
2025-03-24 08:42:05 -0300

    chapter 16 done, course finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,15 @@ import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 // se descarga la fuente, la optimiza y se sirve desde el servidor siendo una fuente local
 
-export const metadata = {
-  title: 'Next.js + TypeScript + Tailwind CSS Starter',
-  description: 'Next.js + TypeScript + Tailwind CSS Starter'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard', // The %s in the template will be replaced with the specific page title.
+    default: 'Acme Dashboard'
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
 }
 
 export default function RootLayout ({
